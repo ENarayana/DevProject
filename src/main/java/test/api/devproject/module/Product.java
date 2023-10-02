bf727b24d7ea76e8f366f8e963b4108fd8205f85
ENarayana <narayanaswamy2@email.com>
2023-10-02 19:04:47 +0530

    Changes In module package

--- src/main/java/test/api/devproject/module/Product.java
+++ src/main/java/test/api/devproject/module/Product.java
@@ -1,15 +1,21 @@
 package test.api.devproject.module;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Entity
+@Data
+@Builder
 public class Product extends BaseModule{
 
     private String title;
     private String description;
     private double price;
     private String image;
+    @ManyToOne
     private Category category;
 }
