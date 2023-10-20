370db162343bf4763f5da0b4a9c234aca5cf65eb
ENarayana <narayanaswamy2@email.com>
2023-10-20 13:18:10 +0530

    Filtarartion Implemented

--- src/main/java/test/api/devproject/services/SelfProductServiceImpl.java
+++ src/main/java/test/api/devproject/services/SelfProductServiceImpl.java
@@ -79,7 +79,6 @@ public class SelfProductServiceImpl implements Productservices {
         Product productUpdate = productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Product Not found: " + id));
 
-        // productUpdate.setId(genericProductDto.getId());
         productUpdate.setImage(genericProductDto.getImage());
         productUpdate.setDescription(genericProductDto.getDescription());
         productUpdate.setTitle(genericProductDto.getTitle());
@@ -128,9 +127,7 @@ public class SelfProductServiceImpl implements Productservices {
 
     @Override
     public GenericProductDto getProductSingle(Long id) {
-//      System.out.println("Received id: " + id);
         Optional<Product> productOptional = productRepository.findById(id);
-//      System.out.println("Product present: " + productOptional.isPresent());
         if (productOptional.isPresent()) {
             Product product = productOptional.get();
 
