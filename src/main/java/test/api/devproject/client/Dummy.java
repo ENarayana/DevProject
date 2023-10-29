241b598d652cc687744816bffd47ce9d1c57fc20
ENarayana <narayanaswamy2@email.com>
2023-10-29 21:50:39 +0530

    kafka consumer implemented

--- src/main/java/test/api/devproject/client/Dummy.java
+++ src/main/java/test/api/devproject/client/Dummy.java
@@ -20,6 +20,7 @@ public class Dummy {
         System.out.println(dummy.year);
         System.out.println("Thank you ");
         System.out.println("Good work");
-        System.out.println("ecommerce");
+
+        System.out.println("Good work");
     }
 }
