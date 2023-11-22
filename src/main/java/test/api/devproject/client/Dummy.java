99b5d73c58b6453d0d2e3479525d151dbb3d3bd6
ENarayana <narayanaswamy2@email.com>
2023-11-22 23:20:47 +0530

    kafka consumer

--- src/main/java/test/api/devproject/client/Dummy.java
+++ src/main/java/test/api/devproject/client/Dummy.java
@@ -21,6 +21,5 @@ public class Dummy {
         System.out.println("Thank you ");
         System.out.println("Good work");
         System.out.println("Have a nice day");
-        System.out.println("Have a nice day");
     }
 }
